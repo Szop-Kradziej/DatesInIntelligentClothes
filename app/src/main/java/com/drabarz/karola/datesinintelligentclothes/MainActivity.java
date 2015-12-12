f0882c01138e3de06375fb776b59618cda69d640
Szop-kradziej <karolina.drabarz@gmail.com>
2015-12-12 23:12:15 +0100

    Add Information button and Snackbar.

--- app/src/main/java/com/drabarz/karola/datesinintelligentclothes/MainActivity.java
+++ app/src/main/java/com/drabarz/karola/datesinintelligentclothes/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +14,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.informationButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(findViewById(R.id.mainLayout), R.string.information, Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+
+
     }
 
     @Override
